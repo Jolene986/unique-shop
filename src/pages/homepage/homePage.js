1ea5899747c8149c1705432e07683f6baf528bd6
Jolene986 <jojovanovic86@gmail.com>
2020-09-01 17:49:06 +0200

    Created react app, cleenup,basic project folders, homepage and shop menu created

---
+++ src/pages/homepage/homePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './homePage.styles.scss'
+import Directory from '../../components/directory/directory'
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+    <Directory/>
+    </div>
+  )
+}
+
+export default HomePage
