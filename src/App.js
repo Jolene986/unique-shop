a108b8794e39b888eac108b80c8a20c661802113
Jolene986 <jojovanovic86@gmail.com>
2020-09-02 15:05:54 +0200

    Navbar created

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homePage";
 import Shop from './pages/shoppage/shop'
 import "./App.css";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={Shop} />
