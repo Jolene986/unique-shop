0e84b6bc31e24fee484e80a218ba4dccb107bfe6
Jolene986 <jojovanovic86@gmail.com>
2020-09-09 11:33:47 +0200

    added and styled quick view icon in collection-item

--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 import './collection-item.styles.scss';
 import CustomButton from '../custom-button/custom-button'
+import {ReactComponent as QuickView} from '../../assets/zoom.svg'
 //REDUX
 import { connect } from 'react-redux'
 import { addToCart } from '../../redux/cart/cart-actions'
@@ -24,6 +25,7 @@ const CollectionItem = ({ item, addToCart }) => {
         <span className="price">{price}&#8364;</span>
       </div>
       <CustomButton inverted onClick={()=>addToCart(item)}>ADD TO CART</CustomButton>
+      <QuickView className='quick-view' onClick={()=>alert('viwe')}/>
     </div>
   );
 }
