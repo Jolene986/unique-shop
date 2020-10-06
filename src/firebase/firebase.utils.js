d1706f37a7d7f45d7c764c843637de72b87604d4
Jolene986 <jojovanovic86@gmail.com>
2020-10-06 15:56:11 +0200

    new firebase config

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,17 +2,27 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASURMENT_ID,
+// const config = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_DATABASE_URL,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+//   measurementId: process.env.REACT_APP_MEASURMENT_ID,
+// };
+const firebaseConfig = {
+  apiKey: "AIzaSyBJOXDBEOQVReWZKaMW_jd_43hTWI8MCGQ",
+  authDomain: "unique-shop-228b3.firebaseapp.com",
+  databaseURL: "https://unique-shop-228b3.firebaseio.com",
+  projectId: "unique-shop-228b3",
+  storageBucket: "unique-shop-228b3.appspot.com",
+  messagingSenderId: "431307137653",
+  appId: "1:431307137653:web:9f1d87f4ba10271cb2ce3c",
+  measurementId: "G-N6T86J1T3Z"
 };
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 // Take user obj and store it into firestore database in users collection
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return; // firebase returns null
